acd42b3f7afbf1e4b5b39b1924cbe4062410c45f
Peter Markushin <p.markushin@itransition.com>
2023-07-18 11:19:00 +0300

    Add cart API integration with AWS EB deployment

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://49x3bleq23.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://49x3bleq23.execute-api.us-east-1.amazonaws.com/prod/v1",
-  cart: "https://hc1da1z0ci.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "https://4q80qn94if.execute-api.us-east-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
